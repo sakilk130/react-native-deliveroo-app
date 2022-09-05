29a3d7d239482183b267a7c79b1f22a96ba09c1b
sakilk130 <sakilk130@gmail.com>
2022-09-06 01:10:35 +0600

    feat: :sparkles: sanity setup

--- components/categories/index.tsx
+++ components/categories/index.tsx
@@ -15,7 +15,7 @@ export default function Categories() {
       {Array.from({ length: 10 }).map((_, index) => (
         <CategoryCard
           key={index}
-          imgUrl="https://links.papareact.com/wru/"
+          imgUrl="https://images.unsplash.com/photo-1617196034796-73dfa7b1fd56?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
           title="Testing 1"
         />
       ))}
